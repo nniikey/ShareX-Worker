38c758ba584c70e697b094e4fc06e0501fedd391
mikey <159828446+nniikey@users.noreply.github.com>
2025-07-13 04:29:20 -0400

    jb2-logo.png

--- client/views/Login.vue
+++ client/views/Login.vue
@@ -49,7 +49,7 @@ apiStore.isPageLoaded = true;
 	<div class="flex min-h-svh flex-col items-center justify-center gap-6 bg-background p-6 md:p-10">
 
 		<div class="w-full max-w-sm">
-			<img src="@/assets/cloud.svg" class="mx-auto w-32 h-32" alt="Cloud">
+			<img src="@/assets/jb2-logo.png" class="mx-auto w-32 h-32" alt="Cloud">
 
 			<Alert variant="destructive" v-show="loginFailed">
 				<Ban class="h-4 w-4"/>
